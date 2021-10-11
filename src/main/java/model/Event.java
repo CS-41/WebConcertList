dd016344cf037c5bfba95ea66e6239283cfe34ba
CS-41 <66191434+CS-41@users.noreply.github.com>
2021-10-11 18:44:37 -0500

    A3
    It's alive!
    
    I got it working - took me a second due to dumb syntax issues. I forgot SQL hates spaces in column names.

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -4,26 +4,65 @@ CIS175 - Fall 2021
 Oct 5, 2021
  */
 package model;
-
 import java.time.LocalDate;
-//Comment, comment,comment.
-public class Event {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+//Note: Feel free to change things - I've adjusted names based on what I think
+//will be easiest to track.
+
+@Entity
+@Table(name="events")
+public class Event {
+	@Id
+	@GeneratedValue
+	@Column(name = "EVENTID")
+	private int id;
+	@Column(name = "EVENTNAME")
 	private String eventName;
-	private LocalDate tripDate;
-	private double costPerTicket;
+	//Changed for clarity and updated the getters/setters/toString
+	@Column(name = "EVENTDATE")
+	private LocalDate eventDate;
+	
+	//Based on her notes, I think the cost per ticket can go into the "TicketSales"
+	//class - which will combine this class with the "Customer Class".
+	//private double costPerTicket;
 	
 	public Event()
 	{
 		super();
 	}
 
-	public Event(String eventName, LocalDate tripDate, double costPerTicket) 
+	public Event(int id, String eventName, LocalDate eventDate) 
+	{
+		super();
+		this.id = id;
+		this.eventName = eventName;
+		this.eventDate = eventDate;
+	}
+	
+	public Event(String eventName, LocalDate eventDate) 
 	{
 		super();
 		this.eventName = eventName;
-		this.tripDate = tripDate;
-		this.costPerTicket = costPerTicket;
+		this.eventDate = eventDate;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	/**
@@ -41,39 +80,26 @@ public class Event {
 	}
 
 	/**
-	 * @return the tripDate
+	 * @return the eventDate
 	 */
-	public LocalDate getTripDate() {
-		return tripDate;
+	public LocalDate getEventDate() {
+		return eventDate;
 	}
 
 	/**
-	 * @param tripDate the tripDate to set
+	 * @param eventDate the eventDate to set
 	 */
-	public void setTripDate(LocalDate tripDate) {
-		this.tripDate = tripDate;
+	public void setEventDate(LocalDate eventDate) {
+		this.eventDate = eventDate;
 	}
 
-	/**
-	 * @return the costPerTicket
-	 */
-	public double getCostPerTicket() {
-		return costPerTicket;
+	public String returnEventDetails() {
+		return this.eventName + ": " + this.eventDate;
 	}
-
-	/**
-	 * @param costPerTicket the costPerTicket to set
-	 */
-	public void setCostPerTicket(double costPerTicket) {
-		this.costPerTicket = costPerTicket;
-	}
-
+		
 	@Override
 	public String toString() {
-		return "Event [eventName=" + eventName + ", tripDate=" + tripDate + ", costPerTicket=" + costPerTicket + "]";
+		return "Event [id=" + id + ", eventName=" + eventName + ", eventDate=" + eventDate + "]";
 	}
-	
-	
-	
-	
+
 }
